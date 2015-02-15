65ffa60b49b4525cc8585377b61d5fb0786f46c3
axton21 <axton_n@yahoo.com>
2015-02-15 14:37:44 -0500

    hmmmm...

--- lib/calf.rb
+++ lib/calf.rb
@@ -1,1 +1,31 @@
-# your code here
+class Calf
+  require 'pry'
+
+  attr_accessor :mother, :lbs_of_milk
+
+
+  def initialize(mother)
+    @mother = mother
+    @lbs_of_milk = 0
+  end
+
+  def hungry?
+    if @lbs_of_milk < 100
+      true
+    else
+      false
+    end
+  end
+
+  def drink_milk
+    if hungry? == true && mother.lbs_of_milk > 50
+      mother.lbs_of_milk -= 50
+      self.lbs_of_milk += 50
+      "nom nom nom..."
+    elsif mother.lbs_of_milk < 50
+      "wish I could"
+    else
+      "No way I'm stuffed!"
+    end
+  end
+end
--- lib/whale.rb
+++ lib/whale.rb
@@ -1,1 +1,51 @@
-# your code here
+class Whale
+  require 'pry'
+
+  attr_reader :name
+  attr_accessor :calves, :lbs_of_milk, :lbs_of_krill
+
+  def initialize(name)
+    @name = name
+    @calves = []
+    @lbs_of_milk = 120
+    @lbs_of_krill = 0
+  end
+
+  def hungry?
+    if self.lbs_of_krill < 2000
+      true
+    else
+      false
+    end
+  end
+
+  def give_birth
+      @calves << Calf.new(self)
+  end
+
+  def adopt_calf(orphan)
+      @calves  << orphan
+      orphan.mother = self
+  end
+
+  def feed_calf(calf)
+    if calf.hungry? == true && self.lbs_of_milk >50
+      calf.lbs_of_milk += 50
+      self.lbs_of_milk -= 50
+    elsif self.lbs_of_milk < 50
+      "not enough milk"
+    elsif calf.hungry? == false
+      "calf isn't hungry"
+    end
+  end
+
+
+
+
+
+
+
+
+
+
+end
